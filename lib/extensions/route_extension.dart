f78dbbd4c7dc6dcf076de37c3a7478588f16cdf1
Akid's Pc <akidfikriazhar@gmail.com>
2025-03-30 04:53:44 +0800

    feat: Enhance `goPush` with parameters
    This commit enhances the `goPush` extension function to accept parameters, aligning its functionality with `GoRouter.goNamed` and `GoRouter.pushNamed`. This allows for passing path parameters, query parameters, extra data, and fragments when navigating, providing greater flexibility and control over navigation within the application.

--- lib/extensions/route_extension.dart
+++ lib/extensions/route_extension.dart
@@ -3,5 +3,25 @@ import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 extension GoRouteExtension on BuildContext {
-  goPush<T>(String route) => kIsWeb ? GoRouter.of(this).goNamed(route) : GoRouter.of(this).pushNamed(route);
+  goPush<T>(
+    String route, {
+    Map<String, String> pathParameters = const <String, String>{},
+    Map<String, dynamic> queryParameters = const <String, dynamic>{},
+    Object? extra,
+    String? fragment,
+  }) =>
+      kIsWeb
+          ? GoRouter.of(this).goNamed(
+              route,
+              pathParameters: pathParameters,
+              queryParameters: queryParameters,
+              extra: extra,
+              fragment: fragment,
+            )
+          : GoRouter.of(this).pushNamed(
+              route,
+              pathParameters: pathParameters,
+              queryParameters: queryParameters,
+              extra: extra,
+            );
 }

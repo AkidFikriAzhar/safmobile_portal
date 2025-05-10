e9bc1635b708365ecdf59845c2b6764e917ea9da
Akid Fikri Azhar <akidfikriazhar@gmail.com>
2025-05-10 13:41:38 +0800

    feat: Integrate BillPliz payment gateway and refactor payment processing
    This commit integrates the BillPliz payment gateway, refactors the payment processing flow, and improves the user experience.
    
    - Replaced BayarCash API integration with BillPliz API for payment processing.
    - Modified `PaymentHelper` to use `BillPlizApi` for creating invoices and redirecting users to the payment URL.
    - Updated `PaymentHelper` to use `BillPlizApi.sandboxBaseUrl` for constructing the payment URL.
    - Removed the payment method selection bottom sheet.
    - Removed `go_router` push to pending payment route.

--- lib/helpers/payment_helper.dart
+++ lib/helpers/payment_helper.dart
@@ -2,12 +2,8 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
-import 'package:safmobile_portal/extensions/route_extension.dart';
 import 'package:safmobile_portal/provider/payment_provider.dart';
-import 'package:safmobile_portal/routes.dart';
-import 'package:safmobile_portal/services/bayarcash_api.dart';
-import 'package:safmobile_portal/widgets/bottomsheet/choose_payment_method_bottomsheet.dart';
+import 'package:safmobile_portal/services/billplz_api.dart';
 import 'package:universal_html/html.dart' as html;
 import 'package:url_launcher/url_launcher.dart';
 // import 'package:universal_html/html.dart' as html;
@@ -31,82 +27,79 @@ class PaymentHelper {
           log('initiate Api Payment Gateway');
 
           // paymentProvider.setLoadingApi(true);
-          final currentPaymentMethod = await showModalBottomSheet<int>(
+          // final currentPaymentMethod = await showModalBottomSheet<int>(
+          //     context: context,
+          //     isScrollControlled: true,
+          //     enableDrag: true,
+          //     builder: (context) {
+          //       return ChangeNotifierProvider(
+          //         create: (BuildContext context) => PaymentProvider(),
+          //         child: ChoosePaymentMethodBottomsheet(),
+          //       );
+          //     });
+
+          showDialog(
               context: context,
-              isScrollControlled: true,
-              enableDrag: true,
+              // barrierDismissible: false,
               builder: (context) {
-                return ChangeNotifierProvider(
-                  create: (BuildContext context) => PaymentProvider(),
-                  child: ChoosePaymentMethodBottomsheet(),
+                return AlertDialog(
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 15),
+                      Text(
+                        'Please wait while we process your payment...',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
                 );
               });
 
-          if (currentPaymentMethod != null) {
-            if (context.mounted) {
-              showDialog(
-                  context: context,
-                  // barrierDismissible: false,
-                  builder: (context) {
-                    return AlertDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CircularProgressIndicator(),
-                          SizedBox(height: 15),
-                          Text(
-                            'Please wait while we process your payment...',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-              // ScaffoldMessenger.of(context).showSnackBar(
-              //   SnackBar(
-              //     backgroundColor: Colors.black.withValues(alpha: 0.7),
-              //     content: Row(
-              //       children: [
-              //         CircularProgressIndicator(),
-              //         SizedBox(width: 10),
-              //         Text('Processing payment...'),
-              //       ],
-              //     ),
-              //   ),
-              // );
+          // if (currentPaymentMethod != null) {
+          //   if (context.mounted) {
+          final paymentId = await BillPlizApi.createInvoice(
+            name: name,
+            email: email,
+            amount: amount,
+            mobile: phone,
+            ticketId: ticketId,
+            userId: uid,
+          );
 
-              final paymentId = await BayarcashApi().createBayarCashPaymentIntent(
-                name: name,
-                email: email,
-                phoneNumber: phone,
-                amount: amount,
-                ticketId: ticketId,
-                paymentMethod: currentPaymentMethod,
-              );
+          // final paymentId = await BayarcashApi().createBayarCashPaymentIntent(
+          //   name: name,
+          //   email: email,
+          //   phoneNumber: phone,
+          //   amount: amount,
+          //   ticketId: ticketId,
+          //   paymentMethod: currentPaymentMethod,
+          // );
 
-              if (context.mounted) {
-                context.pop();
-                final url = '${BayarcashApi.paymentIntentUrlSandbox}$paymentId';
-                if (!kIsWeb) {
-                  launchUrl(Uri.parse(url));
-                } else {
-                  html.window.location.assign(url);
-                }
-                context.goPush(
-                  Routes.pending,
-                  pathParameters: {
-                    'uid': uid,
-                    'ticketId': ticketId,
-                  },
-                  queryParameters: {'paymentId': paymentId},
-                );
-              } else {
-                return;
-              }
+          if (context.mounted) {
+            context.pop();
+            final url = '${BillPlizApi.sandboxBaseUrl}$paymentId';
+            if (!kIsWeb) {
+              launchUrl(Uri.parse(url));
             } else {
-              return;
+              html.window.location.assign(url);
             }
+            // context.goPush(
+            //   Routes.pending,
+            //   pathParameters: {
+            //     'uid': uid,
+            //     'ticketId': ticketId,
+            //   },
+            //   queryParameters: {'paymentId': paymentId},
+            // );
+          } else {
+            return;
           }
+          // } else {
+          //   return;
+          // }
+          // }
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
---
+++ lib/services/billplz_api.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+class BillPlizApi {
+  static const sandboxBaseUrl = 'https://www.billplz-sandbox.com/bills/';
+  static const _sandboxBillApiUrl = 'https://www.billplz-sandbox.com/api/v3/bills';
+  static const _sandboxApiKey = 'bd261a8b-8109-4937-9639-5ced435ced20'; //save it in .env file
+  static const _sandboxCollectionId = '1ds1p46e';
+  static const proxyServer = 'https://cors-anywhere.herokuapp.com/';
+
+  static int _toSen(double ringgit) {
+    return (ringgit * 100).round();
+  }
+
+  static Future<String?> createInvoice({
+    required String name,
+    required String email,
+    required double amount, // Contoh: RM15.00 = 1500
+    required String mobile,
+    required String ticketId,
+    required String userId,
+  }) async {
+    final url = Uri.parse('$proxyServer$_sandboxBillApiUrl');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Authorization': 'Basic ${base64Encode(utf8.encode('$_sandboxApiKey:'))}',
+      },
+      body: {
+        'collection_id': _sandboxCollectionId,
+        'email': email,
+        'name': name,
+        'mobile': mobile,
+        'amount': _toSen(amount).toString(),
+        'callback_url': 'https://safmobile.my',
+        'redirect_url': 'https://portal.safmobile.my/#/docs/$ticketId/$userId/',
+        'description': 'Invoices#$ticketId - Computer and Smartphones Repair Service',
+      },
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+      log('Berjaya cipta invois: ${response.body}');
+      return data['id']; // Link invois
+    } else {
+      log(response.statusCode.toString());
+      throw ('Gagal cipta invois: ${response.body}');
+      // return null;
+    }
+  }
+}
